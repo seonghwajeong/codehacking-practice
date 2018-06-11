5d4a1ac479810fadc996b3d02216596eeeff026f
seonghwajeong <jeong1124@gmail.com>
2018-06-11 15:30:42 +0900

    edit category page reduced row size

--- resources/views/admin/categories/edit.blade.php
+++ resources/views/admin/categories/edit.blade.php
@@ -4,7 +4,7 @@
 
     <h1>Edit Category</h1>
 
-
+    <div class="col-sm-6">
 
         {!! Form::model($category, ['method'=>'PATCH', 'action'=>['AdminCategoriesController@update', $category->id]]) !!}
 
@@ -29,7 +29,7 @@
 
         {!! Form::close() !!}
 
-
+    </div>
 
 
 @stop
